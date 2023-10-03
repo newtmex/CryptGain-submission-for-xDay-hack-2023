f93e3394a75836193d6daec36e0190855891f832
newtmex <brightomex@gmail.com>
2023-10-04 00:02:40 +0100

    Implemented Minting GToken with base pair
    It feels like this implementation brings optimisation funds usage.
    For e.g, the `GTokens` minted in the `mint` test increased and the base pair
    dust on the contract reduced significantly.
    
    I am still wondering if slippage should be handled by the contract, or user
    defined. If we allow user defined, I think it will be fine since all the dust
    generated from each mint are kept by the contract.
    
    Also implementation of slippage on fee ratio might be a good way to go.
    Since, as above, all dusts are kept by the contract, the user might be
    disincentived to effect any malicious activity that this might cause.
    
    Future implementation on the use of dust should include a silent minting of
    GTokens with them. Subsequently, dust GTokens should be used to buy base
    pair tokens and used to reward governance participants. Maybe the base
    pair reserve used for this can be split into 360 parts, each added to the
    governance reward pool each epoch.
    The convertion and reward pool top up should be in one Tx since we will
    want to avoid any confusion from dust base pair and that for governance
    reward pool

--- g_token/src/g_token.rs
+++ g_token/src/g_token.rs
@@ -3,7 +3,10 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
-use pair::{AddLiquidityResultType, ProxyTrait as _};
+use pair::{
+    AddLiquidityResultType, ProxyTrait as _, SwapTokensFixedInputResultType,
+    SwapTokensFixedOutputResultType,
+};
 
 pub mod config;
 pub mod pair_interactions;
@@ -43,60 +46,72 @@ pub trait GToken:
 
     #[endpoint]
     #[payable("*")]
-    fn mint(&self) {
+    fn mint(&self, opt_g_pair: OptionalValue<TokenIdentifier>) {
         let caller = self.blockchain().get_caller();
         let base_pair_id = self.base_pair().get_token_id();
 
         // Set payment swap amount
-        let mut g_pair_payment = self.call_value().single_esdt();
+        let mut sent_payment = self.call_value().single_esdt();
         require!(
-            g_pair_payment.amount >= MIN_MINT_DEPOSIT,
+            sent_payment.amount >= MIN_MINT_DEPOSIT,
             "Insufficient liquidity deposit"
         );
-        g_pair_payment.amount /= 2u64;
-        let g_pair = g_pair_payment.token_identifier.clone();
+        sent_payment.amount /= 2u64;
 
-        // TODO add scenario for base pair token
-        let pair_info = self.get_pair_info(&g_pair);
+        let g_pair_id = opt_g_pair
+            .into_option()
+            .unwrap_or_else(|| sent_payment.token_identifier.clone());
+        require!(
+            g_pair_id != base_pair_id,
+            "Specify GPair ID when minting with base pair"
+        );
+
+        let pair_info = self.get_pair_info(&g_pair_id);
         let call_pair = || self.pair_proxy_obj(pair_info.address.clone());
 
-        // Compute base pair payment
-        let base_pair_payment = {
-            let mut amount_out_min: BigUint<Self::Api> = call_pair()
-                .get_amount_out_view(&g_pair, &g_pair_payment.amount)
-                .execute_on_dest_context();
-            slippage::apply(&mut amount_out_min);
+        // Compute other pair payment
+        let mut amount_out: BigUint<Self::Api> = call_pair()
+            .get_amount_out_view(&sent_payment.token_identifier, &sent_payment.amount)
+            .execute_on_dest_context();
+        slippage::apply(&mut amount_out);
 
-            call_pair()
-                .swap_tokens_fixed_input(&base_pair_id, amount_out_min)
-                .with_esdt_transfer(g_pair_payment.clone())
-                .execute_on_dest_context::<EsdtTokenPayment<Self::Api>>()
+        let (first_payment, second_payment) = if sent_payment.token_identifier == base_pair_id {
+            let g_pair_payment = call_pair()
+                .swap_tokens_fixed_input(&g_pair_id, amount_out)
+                .with_esdt_transfer(sent_payment.clone())
+                .execute_on_dest_context::<SwapTokensFixedInputResultType<Self::Api>>();
+
+            (g_pair_payment, sent_payment)
+        } else {
+            let (base_pair_payment, g_pair_payment_dust) = call_pair()
+                .swap_tokens_fixed_output(&base_pair_id, amount_out)
+                .with_esdt_transfer(sent_payment.clone())
+                .execute_on_dest_context::<SwapTokensFixedOutputResultType<Self::Api>>()
+                .into_tuple();
+
+            self.add_dust(
+                &g_pair_payment_dust.token_identifier,
+                g_pair_payment_dust.amount,
+            );
+
+            (sent_payment, base_pair_payment)
         };
 
-        let (first_send_payment, second_send_payment) = {
-            let mut first_payment = g_pair_payment;
-            let mut second_payment = base_pair_payment;
-            if pair_info.tokens.first_token_id != first_payment.token_identifier {
-                core::mem::swap(&mut first_payment, &mut second_payment);
-            }
-            (first_payment, second_payment)
-        };
-        let first_token_amount_min = slippage::from_ref(&first_send_payment.amount);
+        let first_token_sent_amt = first_payment.amount.clone();
+        let second_token_sent_amt = second_payment.amount.clone();
+
+        let first_token_amount_min = slippage::from_ref(&first_payment.amount);
         let second_token_amount_min = call_pair()
-            .get_equivalent(
-                &first_send_payment.token_identifier,
-                &first_token_amount_min,
-            )
+            .get_equivalent(&first_payment.token_identifier, &first_token_amount_min)
             .execute_on_dest_context::<BigUint<Self::Api>>();
-        let first_send_amt = first_send_payment.amount.clone();
-        let second_send_amt = second_send_payment.amount.clone();
-
-        let (lp_payment, first_payment_dust, second_payment_dust) = call_pair()
+        let (
+            //
+            lp_payment,
+            first_payment_optimal,
+            second_payment_optimal,
+        ) = call_pair()
             .add_liquidity(first_token_amount_min, second_token_amount_min)
-            .with_multi_token_transfer(ManagedVec::from_iter([
-                first_send_payment,
-                second_send_payment,
-            ]))
+            .with_multi_token_transfer(ManagedVec::from_iter([first_payment, second_payment]))
             .execute_on_dest_context::<AddLiquidityResultType<Self::Api>>()
             .into_tuple();
 
@@ -118,7 +133,7 @@ pub trait GToken:
             let mut g_token_payment = self.g_token().mint(g_token_amount);
 
             // Update GToken supplies and distribution values
-            let user_amt = self.add_g_supply(g_pair, &g_token_payment.amount, lp_payment.amount);
+            let user_amt = self.add_g_supply(g_pair_id, &g_token_payment.amount, lp_payment.amount);
             drop(pair_info); // to use after this point, request from storage again;
 
             let mint_fee = &g_token_payment.amount - &user_amt;
@@ -129,12 +144,12 @@ pub trait GToken:
 
         // Update dust for all
         self.add_dust(
-            &first_payment_dust.token_identifier,
-            first_send_amt - first_payment_dust.amount,
+            &first_payment_optimal.token_identifier,
+            first_token_sent_amt - first_payment_optimal.amount,
         );
         self.add_dust(
-            &second_payment_dust.token_identifier,
-            second_send_amt - second_payment_dust.amount,
+            &second_payment_optimal.token_identifier,
+            second_token_sent_amt - second_payment_optimal.amount,
         );
         self.add_dust(&g_payment.token_identifier, mint_fee);
 
--- g_token/tests/all.rs
+++ g_token/tests/all.rs
@@ -320,10 +320,10 @@ fn mint() {
                 .put_account(
                     G_TOKEN_ADDR,
                     CheckAccount::new()
-                        .esdt_balance(LS_TOKEN, "0")
-                        .esdt_balance(LSLP_TOKEN, "408,756")
-                        .esdt_balance(BASE_PAIR, "2,087")
-                        .esdt_balance(G_TOKEN, "4,004,760"),
+                        .esdt_balance(LS_TOKEN, "153")
+                        .esdt_balance(LSLP_TOKEN, "408,759")
+                        .esdt_balance(BASE_PAIR, "556")
+                        .esdt_balance(G_TOKEN, "4,004,763"),
                 )
                 .put_account(
                     user,
@@ -331,7 +331,7 @@ fn mint() {
                         .esdt_balance(LS_TOKEN, "180,000")
                         .esdt_balance(LSLP_TOKEN, "0")
                         .esdt_balance(BASE_PAIR, "0")
-                        .esdt_balance(G_TOKEN, "90,427"),
+                        .esdt_balance(G_TOKEN, "90,489"),
                 ),
         )
         // Second
@@ -347,10 +347,10 @@ fn mint() {
                 .put_account(
                     G_TOKEN_ADDR,
                     CheckAccount::new()
-                        .esdt_balance(LS_TOKEN, "0")
-                        .esdt_balance(LSLP_TOKEN, "418,285")
-                        .esdt_balance(BASE_PAIR, "3,971")
-                        .esdt_balance(G_TOKEN, "4,009,301"),
+                        .esdt_balance(LS_TOKEN, "306")
+                        .esdt_balance(LSLP_TOKEN, "418,294")
+                        .esdt_balance(BASE_PAIR, "985")
+                        .esdt_balance(G_TOKEN, "4,009,310"),
                 )
                 .put_account(
                     user,
@@ -358,7 +358,97 @@ fn mint() {
                         .esdt_balance(LS_TOKEN, "160,000")
                         .esdt_balance(LSLP_TOKEN, "0")
                         .esdt_balance(BASE_PAIR, "0")
-                        .esdt_balance(G_TOKEN, "176,702"),
+                        .esdt_balance(G_TOKEN, "176,882"),
+                ),
+        );
+}
+
+#[test]
+fn mint_with_base_pair() {
+    let mut setup = TestSetup::new();
+    let user = &setup.add_user_address(0u32.into())[..];
+
+    setup.init_contracts();
+
+    setup
+        .world
+        .set_state_step(
+            SetStateStep::new()
+                .put_account(user, Account::new().esdt_balance(BASE_PAIR, "200,000")),
+        )
+        .check_state_step(
+            CheckStateStep::new()
+                .put_account(
+                    G_TOKEN_ADDR,
+                    CheckAccount::new()
+                        .esdt_balance(LS_TOKEN, "0")
+                        .esdt_balance(LSLP_TOKEN, "399,000")
+                        .esdt_balance(G_TOKEN, "4,000,000")
+                        .esdt_balance(BASE_PAIR, "0"),
+                )
+                .put_account(
+                    user,
+                    CheckAccount::new()
+                        .esdt_balance(LS_TOKEN, "0")
+                        .esdt_balance(LSLP_TOKEN, "0")
+                        .esdt_balance(BASE_PAIR, "200,000")
+                        .esdt_balance(G_TOKEN, "0"),
+                ),
+        )
+        // First
+        .sc_call(
+            ScCallStep::new()
+                .from(user)
+                .to(G_TOKEN_ADDR)
+                .function("mint")
+                .argument(LS_TOKEN)
+                .esdt_transfer(BASE_PAIR, 0, "20,000"),
+        )
+        .check_state_step(
+            CheckStateStep::new()
+                .put_account(
+                    G_TOKEN_ADDR,
+                    CheckAccount::new()
+                        .esdt_balance(LS_TOKEN, "0")
+                        .esdt_balance(LSLP_TOKEN, "399,996")
+                        .esdt_balance(BASE_PAIR, "11")
+                        .esdt_balance(G_TOKEN, "4,000,500"),
+                )
+                .put_account(
+                    user,
+                    CheckAccount::new()
+                        .esdt_balance(LS_TOKEN, "0")
+                        .esdt_balance(LSLP_TOKEN, "0")
+                        .esdt_balance(BASE_PAIR, "180,000")
+                        .esdt_balance(G_TOKEN, "9,484"),
+                ),
+        )
+        // Second
+        .sc_call(
+            ScCallStep::new()
+                .from(user)
+                .to(G_TOKEN_ADDR)
+                .function("mint")
+                .argument(LS_TOKEN)
+                .esdt_transfer(BASE_PAIR, 0, "20,000"),
+        )
+        .check_state_step(
+            CheckStateStep::new()
+                .put_account(
+                    G_TOKEN_ADDR,
+                    CheckAccount::new()
+                        .esdt_balance(LS_TOKEN, "0")
+                        .esdt_balance(LSLP_TOKEN, "400,989")
+                        .esdt_balance(BASE_PAIR, "23")
+                        .esdt_balance(G_TOKEN, "4,000,999"),
+                )
+                .put_account(
+                    user,
+                    CheckAccount::new()
+                        .esdt_balance(LS_TOKEN, "0")
+                        .esdt_balance(LSLP_TOKEN, "0")
+                        .esdt_balance(BASE_PAIR, "160,000")
+                        .esdt_balance(G_TOKEN, "18,964"),
                 ),
         );
 }

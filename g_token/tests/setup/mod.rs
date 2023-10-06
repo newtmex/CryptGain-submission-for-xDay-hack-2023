49ddc48f5bcd94bfed3efac7940372966c11b67b
newtmex <brightomex@gmail.com>
2023-10-06 13:37:03 +0100

    BlockState moved to test-utils

--- g_token/tests/setup/mod.rs
+++ g_token/tests/setup/mod.rs
@@ -9,65 +9,7 @@ pub use multiversx_sc_scenario::{
     scenario_model::{Account, BytesKey, BytesValue, ScCallStep, ScDeployStep, SetStateStep},
     ScenarioWorld,
 };
-
-pub struct BlockState {
-    pub rounds_per_epoch: u64,
-    pub info: BlockInfo,
-}
-
-impl BlockState {
-    pub fn new(start_round: u64, rounds_per_epoch: u64) -> Self {
-        let mut info = BlockInfo::new();
-        info.block_round = start_round;
-        info.block_epoch = start_round / rounds_per_epoch;
-
-        Self {
-            rounds_per_epoch,
-            info,
-        }
-    }
-
-    pub fn move_block_round(&mut self, blocks: u64, step: Option<SetStateStep>) -> SetStateStep {
-        let step = match step {
-            Some(step) => step,
-            None => SetStateStep::new(),
-        };
-
-        let BlockInfo {
-            block_round,
-            block_epoch,
-            ..
-        } = &mut self.info;
-
-        *block_round += blocks;
-        *block_epoch = *block_round / self.rounds_per_epoch;
-
-        self.set_and_return_step(step)
-    }
-
-    fn set_and_return_step(&self, step: SetStateStep) -> SetStateStep {
-        step.block_round(self.info.block_round)
-            .block_epoch(self.info.block_epoch)
-    }
-
-    pub fn move_block_epoch(&mut self, epochs: u64, step: Option<SetStateStep>) -> SetStateStep {
-        let step = match step {
-            Some(step) => step,
-            None => SetStateStep::new(),
-        };
-
-        let BlockInfo {
-            block_round,
-            block_epoch,
-            ..
-        } = &mut self.info;
-
-        *block_epoch += epochs;
-        *block_round = *block_epoch * self.rounds_per_epoch;
-
-        self.set_and_return_step(step)
-    }
-}
+use test_utils::block_state::BlockState;
 
 // Wasm Files
 pub const G_TOKEN_WASM: &str = "file:../output/g_token.wasm";
